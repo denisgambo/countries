b2235965ce8ca124147202d2d46a1bbc021df2c8
denisgambo <woussounawan@gmail.com>
2023-03-21 08:25:55 +0000

    countries

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,33 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div v-for="country in countries" class="card" style="width: 18rem;">
+      <img :src="country.flags.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">{{ country.name.common }}</h5>
+        <p class="card-text"> Capitale: {{ country.capital }} <br> Population: {{ country.population }}</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      countries: null
+    }
+  },
+  mounted() {
+    axios.get('https://restcountries.com/v3.1/all')
+      .then((response) => {
+        this.countries = response.data;
+        console.log(response)
+      })
   }
 }
 </script>
